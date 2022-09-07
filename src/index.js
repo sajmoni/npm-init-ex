2e42e5abfc35da874505fd343cfacc17ffa722a4
Simon Lagos <simon.lagos.sallhed@gmail.com>
2022-09-07 22:32:11 +0200

    Sort package.json

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { execa } from "execa";
 import { readPackage } from "read-pkg";
 import { writePackage } from "write-pkg";
+import sortPackageJson from "sort-package-json";
 
 const run = async () => {
   await execa("npm", ["init", "-y"]);
   const packageJson = await readPackage({ normalize: false });
-  const updatedPackageJson = {
+  const updatedPackageJson = sortPackageJson({
     ...packageJson,
     version: "0.0.0",
     license: "MIT",
@@ -16,7 +17,7 @@ const run = async () => {
     engines: {
       node: ">16",
     },
-  };
+  });
   writePackage(updatedPackageJson);
 };
 

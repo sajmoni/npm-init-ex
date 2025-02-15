af641969eed56dbc6606dc8f27b232b5a5e47fcc
Simon Lagos <simon.lagos.sallhed@gmail.com>
2025-02-15 20:04:56 +0100

    Add prettier

--- index.test.ts
+++ index.test.ts
@@ -1,45 +1,45 @@
-import path from "node:path";
-import { readFile } from "node:fs/promises";
+import path from 'node:path'
+import { readFile } from 'node:fs/promises'
 
-import { expect, test } from "vitest";
-import { execa } from "execa";
-import { temporaryDirectory } from "tempy";
-import { readPackage } from "read-pkg";
-import { getBinPath } from "get-bin-path";
+import { expect, test } from 'vitest'
+import { execa } from 'execa'
+import { temporaryDirectory } from 'tempy'
+import { readPackage } from 'read-pkg'
+import { getBinPath } from 'get-bin-path'
 
-test("npm-init-ex", async () => {
-  const binPath = await getBinPath();
+test('npm-init-ex', async () => {
+  const binPath = await getBinPath()
   if (!binPath) {
-    throw new Error("Bin path not found");
+    throw new Error('Bin path not found')
   }
-  const directory = temporaryDirectory({ prefix: "hello-world" });
+  const directory = temporaryDirectory({ prefix: 'hello-world' })
   const { stdout } = await execa(binPath, [], {
     cwd: directory,
     env: {
-      FORCE_COLOR: "2",
+      FORCE_COLOR: '2',
     },
-  });
+  })
 
   const packageJson = await readPackage({
     cwd: directory,
     normalize: false,
-  });
-  const npmrc = await readFile(path.join(directory, ".npmrc"));
-  expect(npmrc.toString()).toEqual("save-exact=true");
+  })
+  const npmrc = await readFile(path.join(directory, '.npmrc'))
+  expect(npmrc.toString()).toEqual('save-exact=true')
 
   // Since the directory name will be different each time, we only assert the prefix part
-  expect(packageJson.name?.includes("hello-world")).toBeTruthy();
+  expect(packageJson.name?.includes('hello-world')).toBeTruthy()
 
-  expect(packageJson.version).toEqual("0.0.0");
-  expect(packageJson.license).toEqual("MIT");
-  expect(packageJson.files).toEqual(["dist/"]);
-  expect(packageJson.main).toEqual("dist/index.js");
-  expect(packageJson.bin).toEqual("dist/index.js");
-  expect(packageJson.type).toEqual("module");
+  expect(packageJson.version).toEqual('0.0.0')
+  expect(packageJson.license).toEqual('MIT')
+  expect(packageJson.files).toEqual(['dist/'])
+  expect(packageJson.main).toEqual('dist/index.js')
+  expect(packageJson.bin).toEqual('dist/index.js')
+  expect(packageJson.type).toEqual('module')
   expect(packageJson.engines).toEqual({
-    node: ">=22",
-  });
+    node: '>=22',
+  })
 
-  console.log("stdout", stdout);
-  expect(stdout).toMatchSnapshot();
-});
+  console.log('stdout', stdout)
+  expect(stdout).toMatchSnapshot()
+})
--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,73 @@
 #!/usr/bin/env node
 
-import { writeFile } from "node:fs/promises";
-import { styleText } from "node:util";
+import { writeFile } from 'node:fs/promises'
+import { styleText } from 'node:util'
 
-import { execa } from "execa";
-import { readPackage } from "read-pkg";
-import { writePackage } from "write-pkg";
-import sortPackageJson from "sort-package-json";
+import { execa } from 'execa'
+import { readPackage } from 'read-pkg'
+import { writePackage } from 'write-pkg'
+import sortPackageJson from 'sort-package-json'
 
 const packageJsonOptions = {
-  version: "0.0.0",
-  license: "MIT",
-  files: ["dist/"],
-  main: "dist/index.js",
-  bin: "dist/index.js",
-  type: "module",
+  version: '0.0.0',
+  license: 'MIT',
+  files: ['dist/'],
+  main: 'dist/index.js',
+  bin: 'dist/index.js',
+  type: 'module',
   engines: {
-    node: ">=22",
+    node: '>=22',
   },
-} as const;
+} as const
 
-console.log();
-console.log(styleText(["blue", "bold"], " npm-init-ex"));
-console.log();
+console.log()
+console.log(styleText(['blue', 'bold'], ' npm-init-ex'))
+console.log()
 
 try {
-  await execa("npm", ["init", "-y"]);
+  await execa('npm', ['init', '-y'])
 
   const packageJson = await readPackage({
     normalize: false,
-  });
+  })
 
   const updatedPackageJson = sortPackageJson({
     ...packageJson,
     ...packageJsonOptions,
-  });
+  })
 
   // @ts-expect-error - sort-package-json doesn't return a compatible type
-  await writePackage(updatedPackageJson);
-  await writeFile(".npmrc", "save-exact=true");
+  await writePackage(updatedPackageJson)
+  await writeFile('.npmrc', 'save-exact=true')
 
-  console.log(` ${styleText("cyan", "package.json")} created`);
+  console.log(` ${styleText('cyan', 'package.json')} created`)
   console.log(
-    `   ${styleText("dim", "Version:")} ${styleText(
-      "bold",
-      packageJsonOptions.version
-    )}`
-  );
+    `   ${styleText('dim', 'Version:')} ${styleText(
+      'bold',
+      packageJsonOptions.version,
+    )}`,
+  )
   console.log(
-    `   ${styleText("dim", "License:")} ${styleText(
-      "bold",
-      packageJsonOptions.license
-    )}`
-  );
+    `   ${styleText('dim', 'License:')} ${styleText(
+      'bold',
+      packageJsonOptions.license,
+    )}`,
+  )
   console.log(
-    `   ${styleText("dim", "Engine:")} ${styleText(
-      "bold",
-      packageJsonOptions.engines.node
-    )}`
-  );
+    `   ${styleText('dim', 'Engine:')} ${styleText(
+      'bold',
+      packageJsonOptions.engines.node,
+    )}`,
+  )
   console.log(
-    `   ${styleText("dim", "Dist folder:")} ${styleText(
-      "bold",
-      packageJsonOptions.files[0]
-    )}`
-  );
+    `   ${styleText('dim', 'Dist folder:')} ${styleText(
+      'bold',
+      packageJsonOptions.files[0],
+    )}`,
+  )
 
-  console.log();
-  console.log(` ${styleText("cyan", ".npmrc")} created`);
+  console.log()
+  console.log(` ${styleText('cyan', '.npmrc')} created`)
 } catch (error) {
-  console.error("Failed to initialize project", error);
+  console.error('Failed to initialize project', error)
 }
